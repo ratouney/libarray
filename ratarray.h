292031d4a10c0b41d9986136a85c38e0da57038d
ratouney <maxime.de-la-fouchardiere@epitech.eu>
2017-01-16 19:03:05 +0100

    Hello

--- ratarray.h
+++ ratarray.h
@@ -12,15 +12,37 @@
 #include <unistd.h>
 #include <string.h>
 
+#define LA_SN 0
+#define LA_SF 1
+#define LA_BN 10
+#define LA_BF 11
+
 struct s_charray;
 
 typedef struct s_charray t_charray;
 
 struct s_charray
 {
-    char **charray;
-
+    char **data;
+    char *fill;
     int size;
 };
 
-char *my_strpaste(char *str, int mode);
+char *my_strpaste(char *,int);
+
+struct s_string;
+
+typedef struct s_string t_string;
+
+struct s_string
+{
+    char *string;
+
+    int (*len)(t_string *);
+    int (*exist)(t_string *, char );
+    int (*cmp)(t_string *, char *, int);
+    char *(*copy)(t_string *, int);
+    char *(*fuse)(t_string *, char *, int);
+    char *(*sl)(t_string *, int, int);
+    void (*show)(t_string *, int);
+};
